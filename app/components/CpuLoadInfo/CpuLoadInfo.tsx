62cd5568aff6ab41e04c0914789498c9b0d48618
wildrun0 <gamesnouth@yandex.ru>
2023-06-01 21:17:23 +0500

    cpuloadinfo implemented

--- app/components/CpuLoadInfo/CpuLoadInfo.tsx
+++ app/components/CpuLoadInfo/CpuLoadInfo.tsx
@@ -5,36 +5,42 @@ import { useEffect, useState } from "react"
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend } from "recharts";
 
 
-const CpuLoadInfo = ({ period }: { period: number }) => {
-	const [cpu_data, add_cpu_data] = useState<cpu_data[]>([]);
+const CpuLoadInfo = () => {
+	const [cpu_info, add_cpu_data] = useState<cpu_data[]>();
 	useEffect(() => {
 		async function get_load(url: string) {
 			const resp = await fetch(url, { cache: "no-store" });
-			let data: cpu_data = await resp.json();
-			add_cpu_data(result => [...result.slice(-period), {
-				time: data.time,
-				temp: data.temp,
-				load: data.load
-			}]);
+			let data: cpu_data[] = await resp.json();
+			data.map((_entry: cpu_data)=>{
+				let date = new Date(_entry.time);
+				let hoursAndMinutes = date.getHours() + ':' + date.getMinutes();
+				_entry.time = hoursAndMinutes;
+			})
+			add_cpu_data(data);
+			console.log(cpu_info)
 		}
 		get_load("/api/cpudata");
-	})
+		let interval = setInterval(() => get_load("/api/cpudata"), 200000);
+		return () => {
+			clearInterval(interval);
+		}
+	}, [add_cpu_data])
 	return (
 		<div className='dashboard-cpu'>
 			<h2>CPU</h2>
-			<div className='dashboard-cpu-info'>
+			{cpu_info && (<div className='dashboard-cpu-info'>
 				<ResponsiveContainer width="100%" height="100%">
-					<LineChart data={cpu_data}>
+					<LineChart data={cpu_info}>
 						<Line dot={false} type="monotone" dataKey="temp" stroke="#FF3131" />
-						<Line dot={false} type="monotone" dataKey="load" stroke="#50C878" />
-						<XAxis dataKey="time" padding={{ right: 10 }} />
+						<Line dot={false} type="monotone" dataKey="usage" stroke="#50C878" />
+						<XAxis dataKey="time" padding={{ right: 10 }}/>
 						<YAxis type="number" domain={[0, 100]} interval={0} padding={{ top: 10 }} />
 						<Tooltip contentStyle={{ backgroundColor: "#1c2137", border: "none" }} />
 						<Legend align='right' layout='vertical' verticalAlign='top' />
 					</LineChart>
 				</ResponsiveContainer>
-				<p className="cpu-since">Since {cpu_data[0].time}</p>
-			</div>
+				<p className="cpu-since">Since {cpu_info[0].time}</p>
+			</div>)}
 		</div>
 	)
 }
